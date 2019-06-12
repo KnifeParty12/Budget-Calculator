4884fe2a4f0340de747a77fd0bce750fbf05aee3
KnifeParty12 <flashfunk01@gmail.com>
2019-06-13 01:35:05 +0530

    Creating init function
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- app.js
+++ app.js
@@ -7,13 +7,23 @@ var budgetController = (function () {
 // UI CONTROLLER
 var UIController = (function () {
 
+    var DOMstrings = {
+        inputType : '.add__type',
+        inputDescription : '.add__description',
+        inputValue : '.add__value',
+        inputBtn : '.add__btn'
+    };
+
     return {
         getinput : function () {
             return {
-                 type :document.querySelector('.add__type').value,   // Will be either inc or exp
-            description: document.querySelector('.add__description').value,
-             value : document.querySelector('.add__value').value
+                 type :document.querySelector(DOMstrings.inputType).value,   // Will be either inc or exp
+            description: document.querySelector(DOMstrings.inputDescription).value,
+             value : document.querySelector(DOMstrings.inputValue).value
         };
+        },
+        getDOMstrings  : function () {
+            return DOMstrings;
         }
     };
 })();
@@ -21,11 +31,25 @@ var UIController = (function () {
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl,UICtrl) {
 
+    var setupEventListeners = function(){
+
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
+        document.addEventListener('keypress',function (event) {
+            if(event.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+            }
+        });
+    };
+
+
+
     var ctrlAddItem = function(){
 
         // 1. Get the field input data
 
-        var input = UIController.getinput();
+        var input = UICtrl.getinput();
         console.log(input);
         // 2. Add the item to the budget controller
 
@@ -36,14 +60,12 @@ var controller = (function (budgetCtrl,UICtrl) {
         // 5. Display the budget to the UI
     };
 
-document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
-
-
-    document.addEventListener('keypress',function (event) {
-
-        if(event.keyCode === 13 || event.which === 13){
-            ctrlAddItem();
+    return {
+        init : function () {
+            setupEventListeners();
         }
-    });
+    }
 
 })(budgetController,UIController);
+
+controller.init();
